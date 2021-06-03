053d7870aa5c5f53d816cc3eafbbc4ed758be294
Ryan Kather <rkather@missionpenguin.com>
2021-06-03 12:30:33 -0400

    simple notebook example with script

---
+++ script.py
@@ -0,0 +1,7 @@
+def ultimate_answer(question):
+  if question != "What is the meaning of life, the universe, everything?":
+    print("You need a better question.")
+  else:
+    print("42")
+
+question = "What is the meaning of life, the universe, everything?"
